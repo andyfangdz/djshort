2955e7c4033c2386bd8a0b33bed225aebfba8015
Andy Fang <andyfang.dz@gmail.com>
2016-01-15 23:49:09 -0500

    add admin

--- redirect/admin.py
+++ redirect/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from redirect.models import Redirect
+
 
 # Register your models here.
+class RedirectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at', 'last_update')
+
+admin.site.register(Redirect, RedirectAdmin)
