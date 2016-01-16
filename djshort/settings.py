f047f439d13b28180d5b7715998962dc9c7c7c9d
Andy Fang <andyfang.dz@gmail.com>
2016-01-16 00:05:20 -0500

    Disable Debug

--- djshort/settings.py
+++ djshort/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0nzu#hx42vo_+o5y=t2r*kj)r$%c#(y%ce)8a@^wbmyzfyjolx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Application definition
