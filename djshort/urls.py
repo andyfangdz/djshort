6a66a3fe1154edac968f77f9f818bae161b81494
Andy Fang <andyfang.dz@gmail.com>
2016-01-16 22:53:46 -0500

    Optional Trailing Slash

--- djshort/urls.py
+++ djshort/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^(?P<slug>.*)$', 'redirect.views.redirect_view')
+    url(r'^admin/?', admin.site.urls),
+    url(r'^(?P<slug>.*)/?$', 'redirect.views.redirect_view')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
