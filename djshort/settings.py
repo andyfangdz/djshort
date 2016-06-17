c90b190b1c0251c942eff12d165f445f22d913f4
Andy Fang <andyfang.dz@gmail.com>
2016-06-17 15:31:52 -0700

    fix DEBUG

--- djshort/settings.py
+++ djshort/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', False))
+DEBUG = os.environ.get('DEBUG', False) == True
 
 
 # Application definition
